469cc0b4520621992a34113e370ea66abf695eac
clairepeng <zdimple0808@gmail.com>
2020-12-20 10:00:09 +0800

    add  onPress event to button

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Hello There! My love</Text>;
+const HomeScreen = ({ navigation }) => {
+  console.warn(navigation);
+  return (
+    <View>
+      <Text style={styles.text}>Hello There! My </Text>
+
+      {/* self-closing element */}
+      <Button
+        onPress={() => navigation.navigate('Components')}
+        title="Go to ComponentScreen"
+      />
+
+      <TouchableOpacity onPress={() => navigation.navigate('List')}>
+        <Text>Go to ListScreen</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
